ed1f4bbd7d747948b8397dedf2325b9468c31a72
Remon <remon_028@outlook.com>
2022-05-31 02:21:31 +0800

    refactor analyzeArray test

--- tests/analyzeArray.test.js
+++ tests/analyzeArray.test.js
@@ -1,10 +1,12 @@
 import analyzeArray from '../js/analyzeArray';
 
 test('[1,2,6] returns {avg: 3, min: 1, max: 6, length: 3}', () => {
-  expect(analyzeArray([2, 6, 1]).avg).toEqual(3);
-  expect(analyzeArray([2, 6, 1]).min).toEqual(1);
-  expect(analyzeArray([2, 6, 1]).max).toEqual(6);
-  expect(analyzeArray([2, 6, 1]).length).toEqual(3);
+  expect(analyzeArray([2, 6, 1])).toEqual({
+    avg: 3,
+    min: 1,
+    max: 6,
+    length: 3,
+  });
 });
 
 test('[] returns null', () => {
