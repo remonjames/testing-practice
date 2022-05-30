565d138ec3978740338e41965bf251e6abf5cecd
Remon <remon_028@outlook.com>
2022-05-31 01:57:05 +0800

    completed caesarCypher.js

---
+++ js/caesarCypher.js
@@ -0,0 +1,23 @@
+const caesarCypher = (str, key) => {
+  const upperKey = 'ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  const lowerKey = 'abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz';
+
+  if (key > 25) {
+    return undefined;
+  }
+
+  return str
+    .split('')
+    .map((char) => {
+      if (lowerKey.includes(char)) {
+        return lowerKey[lowerKey.indexOf(char) + key];
+      } else if (upperKey.includes(char)) {
+        return upperKey[upperKey.indexOf(char) + key];
+      } else {
+        return char;
+      }
+    })
+    .join('');
+};
+
+export default caesarCypher;
---
+++ tests/caesarCipher.test.js
@@ -0,0 +1,17 @@
+import caesarCypher from '../js/caesarCypher';
+
+test('("brutus", 2) becomes "dtwvwu"', () => {
+  expect(caesarCypher('brutus', 2)).toBe('dtwvwu');
+});
+
+test('("Attack On Dawn", 3) becomes "Dwwdfn Rq Gdzq"', () => {
+  expect(caesarCypher('Attack on dawn', 3)).toBe('Dwwdfn rq gdzq');
+});
+
+test('("Don\'t falter!", 3) becomes "Epo\'u gbmufs!"', () => {
+  expect(caesarCypher("Don't falter!", 1)).toBe("Epo'u gbmufs!");
+});
+
+test('("key is greater than 25"', () => {
+  expect(caesarCypher('invalid key', 27)).toBeUndefined();
+});
